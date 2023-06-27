995cd6afd65453efcce1f5c825b5eface807711e
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-27 18:14:04 -0300

    add cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors()); // sem parâmetros no cors, libera para todos os acessos
 app.use(router);
 
 module.exports = app;
